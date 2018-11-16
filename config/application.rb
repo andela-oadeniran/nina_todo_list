5eb2bafebdcf40d8f44a3198caf59a74f46a0bc5
ladiadeniran <oladipupo.adeniran@gmail.com>
2018-11-16 13:26:55 +0100

    handle vulnerabilities

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'app', 'controllers')
-require 'rack'
 require 'nina'
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'app', 'controllers')
 
 module Todolist
   class Application < Nina::Application
